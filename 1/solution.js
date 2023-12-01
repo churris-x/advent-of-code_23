0828348c8d29ce11de9e9f4ac151048ce14b00ff
Francisco Altimari <francisco@altimari.nl>
2023-12-02 00:52:38 +0100

    Day 1 part 2 progress

--- 1/solution.js
+++ 1/solution.js
@@ -24,10 +24,58 @@ console.log('1) input: ', recoverValue(input));
 
 // Part 2 ---------------------------------------------------------------------
 
-// const placeholder = () => {};
+/*
+    jesus christ almighty, number parsing
+    I will try to avoid using regex lol
 
-// console.log('2) eg: ', placeholder(eg));
-// console.log('2) input: ', placeholder(input));
+    this is absolutely cursed
+*/
+
+const fullNumbers = [
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine'
+];
+
+const eg2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+
+const parseRecoverValue = document => document
+    .split('\n')
+    .reduce((sum, line) => {
+        let parsedLine = line;
+
+        while (fullNumbers.some(number => parsedLine.includes(number))) {
+            fullNumbers.forEach((number, index) => {
+                parsedLine = parsedLine.replace(number, index + 1);
+            });
+        }
+
+        console.log(parsedLine);
+
+        const numbers = parsedLine
+            .split('')
+            .filter(letter => !alphabet.includes(letter));
+
+        const number = +[numbers[0], numbers.slice(-1)].join('')
+
+        if (number) return number + sum;
+        return sum;
+    }, 0);
+
+console.log('2) eg: ', parseRecoverValue(eg2));
+// console.log('2) input: ', parseRecoverValue(input));
 
 /*
 Wrong guesses:
