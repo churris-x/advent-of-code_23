94abd8780075cfa401cb50e62a98f33c0bc22672
Francisco Altimari <francisco@altimari.nl>
2023-12-03 09:07:36 +0100

    Solve d3p2

--- 3/solution.js
+++ 3/solution.js
@@ -8,110 +8,212 @@ const alphabet = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '0123456789'
 const symbols = '!@£$%^&*()_+/=-#'
 
-const placeholder = input => input
-        .split('\n')
-        .reduce((sum, line, index, array) => {
+const findValidParts = input => input
+    .split('\n')
+    .reduce((sum, line, index, array) => {
 
-            const prevLine = array[index - 1];
-            const nextLine = array[index + 1];
-
-
-            let currentLine = line;
-
-            const lineNumbers = line
-                .split('.')
-                .flatMap(i => i.split('!'))
-                .flatMap(i => i.split('@'))
-                .flatMap(i => i.split('£'))
-                .flatMap(i => i.split('$'))
-                .flatMap(i => i.split('%'))
-                .flatMap(i => i.split('^'))
-                .flatMap(i => i.split('&'))
-                .flatMap(i => i.split('*'))
-                .flatMap(i => i.split('('))
-                .flatMap(i => i.split(')'))
-                .flatMap(i => i.split('_'))
-                .flatMap(i => i.split('+'))
-                .flatMap(i => i.split('/'))
-                .flatMap(i => i.split('='))
-                .flatMap(i => i.split('-'))
-                .flatMap(i => i.split('#'))
-                .reduce((sum, number) => {
-                    if (!number || !+number) return sum;
-
-                    const i = currentLine.search(number)
-                    const prev = currentLine.substring(0, i)
-                    const next = currentLine.substring(i + number.length, line.length)
-
-                    // console.log();
-                    // console.log(currentLine);
-
-                    currentLine = `${prev}${'.'.repeat(number.length)}${next}`
-
-                    // console.log(currentLine, '\n');
+        const prevLine = array[index - 1];
+        const nextLine = array[index + 1];
 
 
-                    const preffix = '.'.repeat(prev.length)
-                    const suffix = '.'.repeat(next.length)
+        let currentLine = line;
 
-                    return [...sum, `${preffix}${number}${suffix}`];
-                }, [])
+        const lineNumbers = line
+            .split('.')
+            .flatMap(i => i.split('!'))
+            .flatMap(i => i.split('@'))
+            .flatMap(i => i.split('£'))
+            .flatMap(i => i.split('$'))
+            .flatMap(i => i.split('%'))
+            .flatMap(i => i.split('^'))
+            .flatMap(i => i.split('&'))
+            .flatMap(i => i.split('*'))
+            .flatMap(i => i.split('('))
+            .flatMap(i => i.split(')'))
+            .flatMap(i => i.split('_'))
+            .flatMap(i => i.split('+'))
+            .flatMap(i => i.split('/'))
+            .flatMap(i => i.split('='))
+            .flatMap(i => i.split('-'))
+            .flatMap(i => i.split('#'))
+            .reduce((sum, number) => {
+                if (!number || !+number) return sum;
 
-            const validNumbers = lineNumbers.map(number => ([
-                +number.split('.').reduce((sum, i) => i ? [...sum, i] : sum, []),
-                number
-                .split('')
-                .reduce((isPart, char, i) => {
-                    if (isPart) return isPart;
-                    if (!numbers.includes(char)) return isPart;
+                const i = currentLine.search(number)
+                const prev = currentLine.substring(0, i)
+                const next = currentLine.substring(i + number.length, line.length)
 
-                    if (prevLine) {
-                        if (
-                            symbols.includes(prevLine[i - 1]) ||
-                            symbols.includes(prevLine[i]) ||
-                            symbols.includes(prevLine[i + 1])
-                        ) return true;
-                    }
+                currentLine = `${prev}${'.'.repeat(number.length)}${next}`
 
-                    if (nextLine) {
-                        if (
-                            symbols.includes(nextLine[i - 1]) ||
-                            symbols.includes(nextLine[i]) ||
-                            symbols.includes(nextLine[i + 1])
-                        ) return true;
-                    }
+                const preffix = '.'.repeat(prev.length)
+                const suffix = '.'.repeat(next.length)
 
-                    const chars = line.split('')
+                return [...sum, `${preffix}${number}${suffix}`];
+            }, [])
 
-                    const prevChar = chars[i - 1];
-                    const nextChar = chars[i + 1];
+        const validNumbers = lineNumbers.map(number => ([
+            +number.split('.').reduce((sum, i) => i ? [...sum, i] : sum, []),
+            number
+            .split('')
+            .reduce((isPart, char, i) => {
+                if (isPart) return isPart;
+                if (!numbers.includes(char)) return isPart;
 
-                    if (prevChar && symbols.includes(prevChar)) return true
-                    if (nextChar && symbols.includes(nextChar)) return true
+                if (prevLine) {
+                    if (
+                        symbols.includes(prevLine[i - 1]) ||
+                        symbols.includes(prevLine[i]) ||
+                        symbols.includes(prevLine[i + 1])
+                    ) return true;
+                }
 
-                    return false
+                if (nextLine) {
+                    if (
+                        symbols.includes(nextLine[i - 1]) ||
+                        symbols.includes(nextLine[i]) ||
+                        symbols.includes(nextLine[i + 1])
+                    ) return true;
+                }
 
-                }, false)
-            ]));
+                const chars = line.split('')
 
-            const lineSum = validNumbers.reduce((sum, [number, isValid]) => isValid ? sum + number : sum , 0)
+                const prevChar = chars[i - 1];
+                const nextChar = chars[i + 1];
 
-            // console.log(validNumbers, lineSum, sum);
+                if (prevChar && symbols.includes(prevChar)) return true
+                if (nextChar && symbols.includes(nextChar)) return true
 
-            return sum + lineSum;
+                return false
 
-        }, 0)
+            }, false)
+        ]));
+
+        const lineSum = validNumbers.reduce((sum, [number, isValid]) => isValid ? sum + number : sum , 0)
+
+        // console.log(validNumbers, lineSum, sum);
+
+        return sum + lineSum;
+
+    }, 0)
 
 
-console.log('1) eg: ', placeholder(eg));
-console.log('1) input: ', placeholder(input));
+console.log('1) eg: ', findValidParts(eg));
+console.log('1) input: ', findValidParts(input));
 
 // Part 2 ---------------------------------------------------------------------
 
-// const placeholder = () => {};
+const symbol = '*'
+let idSum = {};
 
-// console.log('2) eg: ', placeholder(eg));
-// console.log('2) input: ', placeholder(input));
+
+const findGears = input => input
+    .split('\n')
+    .reduce((sum, line, index, array) => {
+
+        const prevLine = array[index - 1];
+        const nextLine = array[index + 1];
+
+
+        let currentLine = line;
+
+        const lineNumbers = line
+            .split('.')
+            .flatMap(i => i.split('!'))
+            .flatMap(i => i.split('@'))
+            .flatMap(i => i.split('£'))
+            .flatMap(i => i.split('$'))
+            .flatMap(i => i.split('%'))
+            .flatMap(i => i.split('^'))
+            .flatMap(i => i.split('&'))
+            .flatMap(i => i.split('*'))
+            .flatMap(i => i.split('('))
+            .flatMap(i => i.split(')'))
+            .flatMap(i => i.split('_'))
+            .flatMap(i => i.split('+'))
+            .flatMap(i => i.split('/'))
+            .flatMap(i => i.split('='))
+            .flatMap(i => i.split('-'))
+            .flatMap(i => i.split('#'))
+            .reduce((sum, number) => {
+                if (!number || !+number) return sum;
+
+                const i = currentLine.search(number)
+                const prev = currentLine.substring(0, i)
+                const next = currentLine.substring(i + number.length, line.length)
+
+                currentLine = `${prev}${'.'.repeat(number.length)}${next}`
+
+                const preffix = '.'.repeat(prev.length)
+                const suffix = '.'.repeat(next.length)
+
+                return [...sum, `${preffix}${number}${suffix}`];
+            }, [])
+
+        const validNumbers = lineNumbers.map(number => ([
+            +number.split('.').reduce((sum, i) => i ? [...sum, i] : sum, []),
+            number
+            .split('')
+            .reduce(([isPart, id], char, i) => {
+
+                if (isPart) return [isPart, id];
+                if (!numbers.includes(char)) return [isPart, id];
+
+                if (prevLine) {
+                    if (symbol.includes(prevLine[i - 1])) return [true, `${`${index - 1}`.padStart(3, '0')};${`${ i - 1 }`.padStart(3, '0')}`];
+                    if (symbol.includes(prevLine[i]))     return [true, `${`${index - 1}`.padStart(3, '0')};${`${ i }`.padStart(3, '0')}`];
+                    if (symbol.includes(prevLine[i + 1])) return [true, `${`${index - 1}`.padStart(3, '0')};${`${ i + 1 }`.padStart(3, '0')}`];
+                }
+
+                if (nextLine) {
+                    if (symbol.includes(nextLine[i - 1])) return [true, `${`${index + 1}`.padStart(3, '0')};${`${ i - 1  }`.padStart(3, '0')}`];
+                    if (symbol.includes(nextLine[i]))     return [true, `${`${index + 1}`.padStart(3, '0')};${`${ i  }`.padStart(3, '0')}`];
+                    if (symbol.includes(nextLine[i + 1])) return [true, `${`${index + 1}`.padStart(3, '0')};${`${ i + 1  }`.padStart(3, '0')}`];
+                }
+
+                const chars = line.split('')
+
+                const prevChar = chars[i - 1];
+                const nextChar = chars[i + 1];
+
+                if (prevChar && symbol.includes(prevChar)) return [true, `${`${index}`.padStart(3, '0')};${`${i - 1}`.padStart(3, '0')}`]
+                if (nextChar && symbol.includes(nextChar)) return [true, `${`${index}`.padStart(3, '0')};${`${i + 1}`.padStart(3, '0')}`]
+
+                return [false]
+
+            }, [false, ''])
+        ]));
+
+        const lineSum = validNumbers.reduce((sum, [number, [isPart, id]]) => {
+
+            if (!isPart) return sum;
+
+            if (idSum[id]) {
+                idSum[id]++
+            } else {
+                idSum[id] = 1
+            }
+
+            const isValid = idSum[id] <= 2;
+
+            return isValid ? [...sum, [number, id]] : sum;
+        }, [])
+
+        return  [...sum, ...lineSum];
+
+    }, [])
+    .sort((a, b) => a[1].localeCompare(b[1]))
+    .reduce(([sum, product], [number, id]) => {
+
+        // console.log(number);
+
+        if (idSum[id] !== 2) return [sum, 1];
+        if (product !== 1) return [sum + (product * number), 1];
+
+        return [sum, product * number];
+    }, [0, 1])[0];
+
+console.log('2) eg: ', findGears(eg));
+console.log('2) input: ', findGears(input));
 
 /*
 Wrong guesses:
