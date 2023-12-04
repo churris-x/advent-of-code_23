e225446d35a840c720dda15d82e42a3028682b1f
Francisco Altimari <francisco@altimari.nl>
2023-12-04 08:41:08 +0100

    Solve d4p1

--- 4/solution.js
+++ 4/solution.js
@@ -4,10 +4,38 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 
 // Part 1 ---------------------------------------------------------------------
 
-const placeholder = () => {};
+const placeholder = cards => cards
+    .split('\n')
+    .map(card => card
+        .split(':')[1]
+        .split('|')
+        .map(numbers => numbers
+            .split(' ')
+            .filter(i => !!i)
+        )
+    )
+    .reduce((sum, card) => {
+        const [scores, numbers] = card;
 
-// console.log('1) eg: ', placeholder(eg));
-// console.log('1) input: ', placeholder(input));
+
+        const points = numbers.reduce((points, number) => {
+            if (scores.includes(number)) {
+                if (points) return points * 2;
+                return 1;
+            }
+
+            // scores.includes(number) ? points ? 1 : points * 2 : points
+
+            return points
+        }, 0)
+
+        console.log(scores, points);
+
+        return sum + points
+    }, 0);
+
+console.log('1) eg: ', placeholder(eg));
+console.log('1) input: ', placeholder(input));
 
 // Part 2 ---------------------------------------------------------------------
 
@@ -20,6 +48,6 @@ const placeholder = () => {};
 Wrong guesses:
 
 Correct:
-    1) 
+    1) 22193
     2) 
 */
