524a3578cb1114f6c4875a9a980625e76eb0c174
Francisco Altimari <francisco@altimari.nl>
2023-12-05 09:44:12 +0100

    Add d5p1

---
+++ 5/solution.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const eg = fs.readFileSync(require.resolve('./eg.txt')).toString().slice(0, -1);
+const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0, -1);
+
+// Part 1 ---------------------------------------------------------------------
+
+const placeholder = () => {};
+
+// console.log('1) eg: ', placeholder(eg));
+// console.log('1) input: ', placeholder(input));
+
+// Part 2 ---------------------------------------------------------------------
+
+// const placeholder = () => {};
+
+// console.log('2) eg: ', placeholder(eg));
+// console.log('2) input: ', placeholder(input));
+
+/*
+Wrong guesses:
+
+Correct:
+    1) 
+    2) 
+*/
