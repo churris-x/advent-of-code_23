a3b7f950ba098fa9036451ffe38914de03e68d88
Francisco Altimari <francisco@altimari.nl>
2023-12-07 11:22:05 +0100

    D7P1 progress

--- 7/solution.js
+++ 7/solution.js
@@ -6,6 +6,16 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 /*
     if type === type, check first card, then seconds, etc
 
+    const types = [
+        'abxyz',      // High card
+        'AAxyz',      // One pair
+        'AABBx',      // Two pair
+        'AAAxy',      // Three of a kind
+        'AAABB',      // Full house
+        'AAAAx',      // Four of a kind
+        'AAAAA',      // Five of a kind
+    ];
+
     rank compared to other hands
 
     win amount = bid * rank
@@ -14,33 +24,51 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 
              str    int  int   int
     hand  = [cards, bid, type, rank]
+
+    three of a kind:
+        AAAxy    {A: 3, x: 1, y: 1}
+        xAAAy
+        xyAAA
+
 */
 
 const cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
-const types = [
-    'abxyz',      // High card
-    'AAxyz',      // One pair
-    'AABBx',      // Two pair
-    'AAAxy',      // Three of a kind
-    'AAABB',      // Full house
-    'AAxAA',      // Four of a kind
-    'AAAAA',      // Five of a kind
-];
 
 const getWinnings = bets => bets
     .split('\n')
-    .map(i => {
-        return i.split(' ')
-    })  // calc type
-    .sort((a, b) => {
-        if (b[2] - a[2] !== 0) return b[2] - a[2];  // sort by highest type
+    .map(bet => {
+        const [cards, bid] = bet.split(' ');
 
-        // order same type cards somehow
-        return b[2] - a[2]
-    })
-    .reduce((sum, [hand, bid, type], rank) => {
-        // calc winnings
-    }, 0)
+        const types = Object.entries(cards.split('').reduce((dictionary, card) => ({
+            ...dictionary,
+            [card]: (dictionary[card] ?? 0) + 1}
+        ), {}))
+
+
+        // if ( types.find(5) ) return five of a kind
+        // if ( types.find(4) ) return four of a kind
+
+        // filter
+        // if ( types.find(3) && anotherTypes.find(2)) return full house
+        // if ( types.find(3)) return three of a kind house
+
+        // filter
+        // if ( types.find(2) && anotherTypes.find(2) ) return two pair
+        // if ( types.find(2) ) reutrn one pair
+
+        // return high card
+
+        return [cards, bid, types]
+    })  // calc type
+//     .sort((a, b) => {
+//         if (b[2] - a[2] !== 0) return b[2] - a[2];  // sort by highest type
+//
+//         // order same type cards somehow
+//         return b[2] - a[2]
+//     })
+//     .reduce((sum, [hand, bid, type], rank) => {
+//         // calc winnings
+//     }, 0)
 
 console.log('1) eg: ', getWinnings(eg));
 // console.log('1) input: ', placeholder(input));
