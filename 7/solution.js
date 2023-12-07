a9530acb582580bf0a436ec36b5e9cb0de2bad82
Francisco Altimari <francisco@altimari.nl>
2023-12-07 08:35:18 +0100

    D7p1 progress

--- 7/solution.js
+++ 7/solution.js
@@ -9,10 +9,15 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
     rank compared to other hands
 
     win amount = bid * rank
+
+    Ohhh this is not as easy as I thought.... will have to re-iterate
+
+             str    int  int   int
+    hand  = [cards, bid, type, rank]
 */
 
 const cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
-const ranks = [
+const types = [
     'abxyz',      // High card
     'AAxyz',      // One pair
     'AABBx',      // Two pair
@@ -24,8 +29,16 @@ const ranks = [
 
 const placeholder = bets => bets
     .slice('\n')
-    .map(i => i.split(' '))
-    .reduce((sum, []) => )
+    .map(i => i.split(' '))  // calc type
+    .sort((a, b) => {
+        if (b[2] - a[2] !== 0) return b[2] - a[2];  // sort by highest type
+
+        // order same type cards somehow
+        return b[2] - a[2]
+    })
+    .reduce((sum, [hand, bid, type], rank) => {
+        // calc winnings
+    }, 0)
 
 console.log('1) eg: ', placeholder(eg));
 // console.log('1) input: ', placeholder(input));
