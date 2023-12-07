a4d46e4445cf866e76e217e3e9e6a99b06ae1db9
Francisco Altimari <francisco@altimari.nl>
2023-12-07 08:26:38 +0100

    Progress d7p1

--- 7/solution.js
+++ 7/solution.js
@@ -3,10 +3,29 @@ const eg = fs.readFileSync(require.resolve('./eg.txt')).toString().slice(0, -1);
 const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0, -1);
 
 // Part 1 ---------------------------------------------------------------------
+/*
+    if type === type, check first card, then seconds, etc
+
+    rank compared to other hands
+
+    win amount = bid * rank
+*/
 
 const cards = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
+const ranks = [
+    'abxyz',      // High card
+    'AAxyz',      // One pair
+    'AABBx',      // Two pair
+    'AAAxy',      // Three of a kind
+    'AAABB',      // Full house
+    'AAxAA',      // Four of a kind
+    'AAAAA',      // Five of a kind
+];
 
-const placeholder = () => {};
+const placeholder = bets => bets
+    .slice('\n')
+    .map(i => i.split(' '))
+    .reduce((sum, []) => )
 
 console.log('1) eg: ', placeholder(eg));
 // console.log('1) input: ', placeholder(input));
