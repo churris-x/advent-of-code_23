9033103660a5761b4242dceb1a91e15af79a3a55
Francisco Altimari <francisco@altimari.nl>
2023-12-08 06:11:49 +0100

    Add D8P1 puzzle

---
+++ 8/solution.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const eg = fs.readFileSync(require.resolve('./eg.txt')).toString().slice(0, -1);
+const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0, -1);
+
+// Part 1 ---------------------------------------------------------------------
+/*
+ I will need to create some memoization here
+
+
+
+*/
+
+
+const placeholder = input => {
+    const [movement, nodes] = input.split('\n\n').map(i => i.split('\n'))
+
+    const result = {
+
+    }
+
+
+
+    return movement
+};
+
+console.log('1) eg: ', placeholder(eg));
+// console.log('1) input: ', placeholder(input));
+
+// Part 2 ---------------------------------------------------------------------
+
+// const placeholder = () => {};
+
+// console.log('2) eg: ', placeholder(eg));
+// console.log('2) input: ', placeholder(input));
+
+/*
+Wrong guesses:
+
+Correct:
+    1) 
+    2) 
+*/
