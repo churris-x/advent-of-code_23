e6a0be986d8f49129d74996317bd75af036a591a
Francisco Altimari <francisco@altimari.nl>
2023-12-08 09:21:06 +0100

    Cleanup Day 8

--- 8/solution.js
+++ 8/solution.js
@@ -5,23 +5,20 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 // Part 1 ---------------------------------------------------------------------
 /*
     node['L'] = 'BBB';
+
+    Starting node is always AAA, not the first node in the list
 */
 
 const stepsToNode = input => {
-    const [movement, rawNodes] = input.split('\n\n').map(i => i.split('\n'))
+    const [movement, rawNodes] = input.split('\n\n').map(i => i.split('\n'));
 
-    const steps = movement[0] //.split('');
+    const steps = movement[0];
     const nodes = rawNodes
         .map(i => i.replaceAll(/\(|\)|\,/gi, '').split(' '))
-        .reduce((nodes, node) => ({...nodes, [node[0]]: {L: node[2], R: node[3] }}), {})
+        .reduce((nodes, node) => ({...nodes, [node[0]]: {L: node[2], R: node[3] }}), {});
 
     const moveNode = (node, step) => nodes[node][step];
-    const cache = {};
 
-    // let currentNode = rawNodes[0].slice(0, 3);
-    // const lastNode = rawNodes.slice(-1)[0].slice(0, 3)
-
-    // this feels stupid haha
     let currentNode = 'AAA';
     const lastNode = 'ZZZ';
 
@@ -42,14 +39,21 @@ console.log('1) eg: ', stepsToNode(eg));
 console.log('1) input: ', stepsToNode(input));
 
 // Part 2 ---------------------------------------------------------------------
+/*
+    Took a while to figure out the lowest common multiple
+
+    gcd => if the modulus is 0, then you have the right number
+
+    keep comparing the lcm, until go through all
+*/
 
 const placeholder = input => {
-    const [movement, rawNodes] = input.split('\n\n').map(i => i.split('\n'))
+    const [movement, rawNodes] = input.split('\n\n').map(i => i.split('\n'));
 
-    const steps = movement[0] //.split('');
+    const steps = movement[0];
     const nodes = rawNodes
         .map(i => i.replaceAll(/\(|\)|\,/gi, '').split(' '))
-        .reduce((nodes, node) => ({...nodes, [node[0]]: {L: node[2], R: node[3] }}), {})
+        .reduce((nodes, node) => ({...nodes, [node[0]]: {L: node[2], R: node[3] }}), {});
 
     const findSteps = (startNode) => {
         const moveNode = (node, step) => nodes[node][step];
@@ -67,7 +71,7 @@ const placeholder = input => {
         }
 
         return [startNode, currentNode, index];
-    }
+    };
 
     // https://en.wikipedia.org/wiki/Least_common_multiple
     // lcm(a, b) = a / gcd(a, b) * b
@@ -76,8 +80,8 @@ const placeholder = input => {
     const lcm = (a, b) => a / gcd(a, b) * b;
 
     const nodeSteps = Object.keys(nodes)
-        .reduce((array, node) => node[2] === 'A' ? [...array, findSteps(node)[2]] : array, [])
-        .reduce(lcm, 1)
+        .reduce((array, node) => node[2] === 'A' ? [...array, findSteps(node)] : array, [])
+        .reduce((multiple, node) => lcm(multiple, node[2]), 1);
 
     return nodeSteps;
 };
