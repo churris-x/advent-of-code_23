29f7843dbca69c9b1c7388b17c2da44b2871e2b3
Francisco Altimari <francisco@altimari.nl>
2023-12-09 11:16:53 +0100

    D9P1 bruh

--- 9/solution.js
+++ 9/solution.js
@@ -3,7 +3,12 @@ const eg = fs.readFileSync(require.resolve('./eg.txt')).toString().slice(0, -1);
 const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0, -1);
 
 // Part 1 ---------------------------------------------------------------------
+/*
+    negative numbers as well! should the amount be positive?
 
+    bruh
+    14 13 12 11 10 9 8 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -6
+*/
 const placeholder = input => input;
 
 console.log('1) eg: ', placeholder(eg));
