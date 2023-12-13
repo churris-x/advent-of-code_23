a971304dcab59a08897125e2d2b041edd66a3ce3
Francisco Altimari <francisco@altimari.nl>
2023-12-13 18:39:16 +0100

    Add D10P1 example

--- 10/solution.js
+++ 10/solution.js
@@ -3,6 +3,30 @@ const eg = fs.readFileSync(require.resolve('./eg.txt')).toString().slice(0, -1);
 const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0, -1);
 
 // Part 1 ---------------------------------------------------------------------
+/*
+    Change all letters to box drawing chars
+
+    ╴    WEST
+    ╵    NORTH
+    ╶    EAST
+    ╷    SOUTH
+
+    ─    EAST WEST
+    │    NORTH SOUTH
+    ┌    EAST SOUTH
+    ┐    SOUTH WEST
+    └    NORTH EAST
+    ┘    WEST NORTH
+
+    ├
+    ┤
+    ┬
+    ┴
+
+    ┼
+*/
+
+const inputSymbols = {'|': 0, '-': 0, 'L': 0, 'J': 0, 7: 0, 'F': 0};
 
 const placeholder = input => input;
 
