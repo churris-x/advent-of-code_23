f67ea96443e3f8e8750cbfe47255c03ae2162859
Francisco Altimari <francisco@altimari.nl>
2023-12-14 08:50:54 +0100

    D10P1 render box drawing characters

--- 10/solution.js
+++ 10/solution.js
@@ -6,17 +6,21 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
 /*
     Change all letters to box drawing chars
 
-    ╴    WEST
-    ╵    NORTH
-    ╶    EAST
-    ╷    SOUTH
+    const [N, E, S, W] = [0, 1, 2, 4]
 
-    ─    EAST WEST
-    │    NORTH SOUTH
-    ┌    EAST SOUTH
-    ┐    SOUTH WEST
-    └    NORTH EAST
-    ┘    WEST NORTH
+    NESW
+
+    ╵    NORTH => N---
+    ╶    EAST  => -E--
+    ╷    SOUTH => --S-
+    ╴    WEST  => ---W
+
+    ─    EAST WEST   => -E-W
+    │    NORTH SOUTH => N-S-
+    ┌    EAST SOUTH  => -ES-
+    ┐    SOUTH WEST  => --SW
+    └    NORTH EAST  => NE--
+    ┘    WEST NORTH  => N--W
 
     ├
     ┤
@@ -26,12 +30,26 @@ const input = fs.readFileSync(require.resolve('./input.txt')).toString().slice(0
     ┼
 */
 
+const [eg0, eg1] = eg.split('\n\n');
+
 const inputSymbols = {'|': 0, '-': 0, 'L': 0, 'J': 0, 7: 0, 'F': 0};
 
-const placeholder = input => input;
+const placeholder = input => input
+    .replaceAll('|', '│')
+    .replaceAll('-', '─')
+    .replaceAll('L', '└')
+    .replaceAll('J', '┘')
+    .replaceAll('7', '┐')
+    .replaceAll('F', '┌')  // ahhh much better
 
-console.log('1) eg: ', placeholder(eg));
-// console.log('1) input: ', placeholder(input));
+console.log('1) eg0: ');
+console.log(placeholder(eg0));
+console.log('\n');
+console.log('1) eg1: ');
+console.log(placeholder(eg1));
+console.log('\n');
+console.log('1) input: ');
+// console.log(placeholder(input));
 
 // Part 2 ---------------------------------------------------------------------
 
